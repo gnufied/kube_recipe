c8c72a46935dd2c0c9c7faf6bb0f8a4609988348
Hemant Kumar <gethemant@gmail.com>
2016-09-07 18:37:43 -0400

    Things are compiling again

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 dnf update -y
-dnf install -y etcd kubernetes golang git docker mercurial wget ruby openssh-server iptables flannel
+dnf install -y etcd kubernetes golang git docker mercurial wget ruby openssh-server iptables docker flannel
 
 #systemctl disable iptables-services firewalld
 #systemctl stop iptables-services firewalld
